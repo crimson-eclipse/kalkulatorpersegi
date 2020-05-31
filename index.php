9a8a476b1bb58e35938acb393fada7c8d166bfc3
Samuel Eljoy <54344839+crimson-eclipse@users.noreply.github.com>
2020-05-31 15:33:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span> 
 					</button>
-                    <a class="navbar-brand" href="/"><img src="images/katik-logo.png" alt="Katik Logo" title="Katik - Kalkulator Matematik"/></a>
+                    <a class="navbar-brand" href="/"><img src="images/katik-logo.png" alt="Katik Logo" title="KatikSam - Kalkulator Matematik"/></a>
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 					<ul class="nav navbar-nav navbar-right">
